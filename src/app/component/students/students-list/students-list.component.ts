a012886783ee920940460f7554e72521f73086f8
Shehan <shehansalitha1999@gmail.com>
2024-08-27 15:25:29 +0530

    implememt filter option

--- src/app/component/students/students-list/students-list.component.ts
+++ src/app/component/students/students-list/students-list.component.ts
@@ -11,18 +11,33 @@ import { StudentsService } from 'src/app/Services/students.service';
 export class StudentsListComponent implements OnInit {
 
   students:Student[]=[]
+  searchTerm: string = '';
+  filteredStudents: any[] = [];
   constructor(private studentsService:StudentsService) { }
 
+ 
+
   ngOnInit(): void {
+ 
       this.studentsService.getAllStudents()
       .subscribe({
         next:(students)=>{
         this.students=students
+        this.filteredStudents=students
         },
         error:(response)=>{
           console.log(response)
         }
       })
   }
-
+  searchStudent(): void {
+    if (this.searchTerm) {
+      this.filteredStudents = this.students.filter(student => 
+        student.std.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredStudents = this.students;
+    }
+  }
+  
 }
